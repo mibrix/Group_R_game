6a6530f6c38d7b88658d942ce6a2a023fa349493
ondre <ondrejga@gmail.com>
2023-09-18 15:54:10 +0200

    Commit 5

--- Board.py
+++ Board.py
@@ -1,11 +1,16 @@
 class Board:
 
     def __init__(self):
-        self.boardRepresentation = {'A1' : ('E',['A4','D1']),
-                                    'D1' : ('E',['A1','G1','D2'])}
-        self.historyOfMoves = []
+        self.boardRepresentation = {'A1' : ('E',['A4','D1']),       #finish this variable based on picture of coordinate system
+                                    'D1' : ('E',['A1','G1','D2'])}  #if player with black pieces place a piece to D1 then change
+                                                                    # "E" to "B"
+                                                                    # elif player with white pieces place a piece to D1 then change
+                                                                    # "E" to "W"
+                                                                    # E = empty; B = black; W = white
+        self.historyOfMoves = []    #use this variable to evaluate if the mill counts
+                                    #you can update this variable in method movePiece
 
-    def isTheMoveLegal(self, initialPosition : str, moveTo : str) -> bool:
+    def isTheMoveLegal(self, initialPosition : str, moveTo : str) -> bool:  #if player plays invalid move: return False else True
         return False
 
     def movePiece(self, initialPosition : str, moveTo : str) -> bool:
--- Game.py
+++ Game.py
@@ -4,14 +4,17 @@ from Board import Board
 class Game:
 
     def __init__(self, board : Board()):
-        self.playerOnTurn = 1
-        self.gamePhase = 'initial'
-        self.numberOfTurns = 0
+        self.playerOnTurn = 1   #player1 or player2
+        self.gamePhase = 'initial'  #initial(when players put the pieces from "pocket"
+                                    #late = after all pieces have been placed to the board
+        self.numberOfTurns = 0      #incriment after every turn
         self.playersList : list[Player] = [Player(1,'black', board), Player(2,'white', board)]
         self.board = board
 
     def play(self, playerOnTurn:int, gamePhase:str, moveTo:str) -> str:
+        #call appropriate method for player on turn
         return 'dummy string'
 
     def isTheGameFinished(self, numberOfPiecesP1 : int, numberOfPiecesP2 : int) -> bool:
+        #check if the game is finished
         return True
